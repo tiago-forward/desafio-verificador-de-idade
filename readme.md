1a526731cfc2156d6652596b7b296db0efe5eda6
Tiago Forward <devtiagoforward@gmail.com>
2023-12-28 17:06:07 -0300

    refatorando-desafio

---
+++ readme.md
@@ -0,0 +1,45 @@
+# Desafio Curso em Vídeo | Verificador de idade
+
+## Tabela do Conteudo
+
+<ul>
+<li><a href="#sobre">Sobre</a></li>
+<li><a href="#status">Status</a></li>
+<li><a href="#objetivo">Objetivo</a></li>
+<li><a href="#curiosidade">Curiosidade</a></li>
+<li><a href="#tecnologias">Tecnologias</a></li>
+<li><a href="#acessando-o-site">Acessando o site</a></li>
+</ul>
+
+## Sobre
+
+### Layout Desktop | Mobile.
+<img src="src/gif/verificador-idade-descktop.gif" alt="Gif do layout desktop" width=950px>
+<img src="src/imagens/verificador-idade-mobile.png" alt="Imagem do layout mobile" width=262px>
+
+## Status
+
+Concluído! 👌
+
+## Objetivo
+
+Depois de concluir o módulo de JavaScript do Curso em Video, esse é o segundo desafio proposto do curso, desenvolver um site que pudesse adicionar um ano de nascimento, selecionar se o sexo é masculino ou feminino, e exibir na tela o ano atual do sexo selecionado com uma imagem de fundo, utilizando html, css e principalmente javascript.
+
+## Curiosidade
+
+Nessa estrutura, é verificado o ano de nascimento, caso não tenha sido preenchido vai apresentar um erro na tela, o sexo selecionado e irá adicionar também um estilo de BackgroundImage diretamente do JavaScript com a url da imagem! 
+
+Antes de olhar qualquer resolução desse exercício, fiz minha primeira solução básica para resolver esse problema, na qual já foi enviada anteriormente ao meu repositório do GitHub, e agora estou refatorando algumas linhas de código.
+
+## Tecnologias
+
+<ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>JavaScript</li>
+<li>GIT BASH</li>
+</ul>
+
+## Acessando o site
+
+Para acessar a página, por favor use esse link : <a href="https://tiago-forward.github.io/desafio-verificador-de-idade/" target="_blank">Desafio Curso em Vídeo | Verificador de idade</a>
--- src/js/index.js
+++ src/js/index.js
@@ -4,39 +4,34 @@ const descricaoDaPessoa = document.querySelector('#descricao')
 const sexoMasculino = document.querySelector('#sexo-masculino')
 const sexoFeminino = document.querySelector('#sexo-feminino')
 const anoAtual = new Date()
-
 const imagemPessoa = document.querySelector('#img-pessoa')
 
 btnDeVerificar.addEventListener('click', () => {
-    
     const idade = anoAtual.getFullYear() - anoDeNascimento.value
-    let sexoSelecionado = "";
-    
-    if (sexoMasculino.checked) {
-        sexoSelecionado = 'Homem'
-        descricaoDaPessoa.textContent = (`Detectamos ${sexoSelecionado} com ${idade} anos.`)
+    imagemPessoa.style.backgroundImage = ''
+
+    if (anoDeNascimento.value == 0 || anoDeNascimento.value > anoAtual.getFullYear()) {
+        alert('[ERRO] Preencha os dados corretamente!')
+        imagemPessoa.style.display = 'none'
+    } else if (sexoMasculino.checked) {
+        imagemPessoa.style.display = 'block'
+        descricaoDaPessoa.textContent = (`Detectamos um Homem com ${idade} anos.`)
         if (idade < 18) {
             imagemPessoa.style.backgroundImage = "url('src/imagens/crianca_homem.jpg')"
-            imagemPessoa.style.display = 'block'
         } else if (idade < 40) {
             imagemPessoa.style.backgroundImage = "url('src/imagens/adulto_homem.jpg')"
-            imagemPessoa.style.display = 'block'
         } else {
             imagemPessoa.style.backgroundImage = "url('src/imagens/idoso_homem.jpg')"
-            imagemPessoa.style.display = 'block'
         }
     } else if (sexoFeminino.checked) {
-        sexoSelecionado = 'Mulher'
-        descricaoDaPessoa.textContent = (`Detectamos ${sexoSelecionado} com ${idade} anos.`)
+        imagemPessoa.style.display = 'block'
+        descricaoDaPessoa.textContent = (`Detectamos uma Mulher com ${idade} anos.`)
         if (idade < 18) {
             imagemPessoa.style.backgroundImage = "url('src/imagens/crianca_mulher.jpg')"
-            imagemPessoa.style.display = 'block'
         } else if (idade < 40) {
             imagemPessoa.style.backgroundImage = "url('src/imagens/adulto_mulher.jpg')"
-            imagemPessoa.style.display = 'block'
         } else {
             imagemPessoa.style.backgroundImage = "url('src/imagens/idoso_mulher.jpg')"
-            imagemPessoa.style.display = 'block'
         }
     }
 })
